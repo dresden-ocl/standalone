bc3bc99c7a5db8d6547cd8a92e148b97b87f9b16
Hammad <hammadirshad046@gmail.com>
2015-11-30 11:25:05 +0100

    gy

--- org.dresdenocl.standalone.example/src/tudresden/ocl20/pivot/standalone/example/StandaloneDresdenOCLExample.java
+++ org.dresdenocl.standalone.example/src/tudresden/ocl20/pivot/standalone/example/StandaloneDresdenOCLExample.java
@@ -4,64 +4,60 @@ import java.io.File;
 import java.net.URL;
 import java.util.List;
 
+import org.dresdenocl.interpreter.IInterpretationResult;
 import org.dresdenocl.metrics.OclMetrics;
+import org.dresdenocl.model.IModel;
+import org.dresdenocl.modelinstance.IModelInstance;
+import org.dresdenocl.modelinstancetype.java.internal.modelinstance.JavaModelInstance;
+import org.dresdenocl.pivotmodel.Constraint;
+import org.dresdenocl.standalone.facade.StandaloneFacade;
+import org.dresdenocl.tools.codegen.declarativ.IOcl2DeclSettings;
+import org.dresdenocl.tools.codegen.declarativ.Ocl2DeclCodeFactory;
+import org.dresdenocl.tools.codegen.declarativ.impl.Ocl2DeclSettings;
+import org.dresdenocl.tools.codegen.ocl2java.IOcl2JavaSettings;
+import org.dresdenocl.tools.codegen.ocl2java.Ocl2JavaFactory;
+import org.dresdenocl.tools.template.TemplatePlugin;
+
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
-
-import tudresden.ocl20.pivot.examples.pml.PmlPackage;
-import tudresden.ocl20.pivot.examples.simple.Person;
-import tudresden.ocl20.pivot.examples.simple.Professor;
-import tudresden.ocl20.pivot.examples.simple.Student;
-import tudresden.ocl20.pivot.interpreter.IInterpretationResult;
-import tudresden.ocl20.pivot.model.IModel;
-import tudresden.ocl20.pivot.modelinstance.IModelInstance;
-import tudresden.ocl20.pivot.modelinstancetype.java.internal.modelinstance.JavaModelInstance;
-import tudresden.ocl20.pivot.pivotmodel.Constraint;
-import tudresden.ocl20.pivot.standalone.facade.StandaloneFacade;
-import tudresden.ocl20.pivot.tools.codegen.declarativ.IOcl2DeclSettings;
-import tudresden.ocl20.pivot.tools.codegen.declarativ.Ocl2DeclCodeFactory;
-import tudresden.ocl20.pivot.tools.codegen.declarativ.impl.Ocl2DeclSettings;
-import tudresden.ocl20.pivot.tools.codegen.ocl2java.IOcl2JavaSettings;
-import tudresden.ocl20.pivot.tools.codegen.ocl2java.Ocl2JavaFactory;
-import tudresden.ocl20.pivot.tools.template.TemplatePlugin;
 
 public class StandaloneDresdenOCLExample {
 
 	final static File rlModel = new File("resources/model/royalsandloyals.uml");
 	final static File rlInstance = new File(
 			"resources/modelinstance/tudresden/ocl20/pivot/examples/royalsandloyals/instance/ModelInstanceProviderClass.class");
-	final static File rlOclConstraints = new File(
-			"resources/constraints/rl_allConstraints.ocl");
+	final static File rlOclConstraints =
+			new File("resources/constraints/rl_allConstraints.ocl");
 
 	final static File pmlModel = new File("resources/model/pml.ecore");
-	final static File pmlInstance = new File(
-			"resources/modelinstance/goodModelInstance.pml");
-	final static File pmlOclConstraints = new File(
-			"resources/constraints/pml_wfrs.ocl");
+	final static File pmlInstance =
+			new File("resources/modelinstance/goodModelInstance.pml");
+	final static File pmlOclConstraints =
+			new File("resources/constraints/pml_wfrs.ocl");
 
 	final static File simpleModel = new File(
 			"resources/model/tudresden/ocl20/pivot/examples/simple/ModelProviderClass.class");
 	final static File simpleInstance = new File(
 			"resources/modelinstance/tudresden/ocl20/pivot/examples/simple/instance/ModelInstanceProviderClass.class");
-	final static File simpleOclConstraints = new File(
-			"resources/constraints/simple_allConstraints.ocl");
+	final static File simpleOclConstraints =
+			new File("resources/constraints/simple_allConstraints.ocl");
 
-	final static File painModel = new File(
-			"resources/model/pain.008.001.01corrected.xsd");
-	final static File painInstance = new File(
-			"resources/modelinstance/BusEx1.xml");
-	final static File painOclConstraints = new File(
-			"resources/constraints/pain_wfrs.ocl");
+	final static File painModel =
+			new File("resources/model/pain.008.001.01corrected.xsd");
+	final static File painInstance =
+			new File("resources/modelinstance/BusEx1.xml");
+	final static File painOclConstraints =
+			new File("resources/constraints/pain_wfrs.ocl");
 
-	final static File universityModel = new File(
-			"resources/model/university_complex.uml");
-	final static File universityConstraints = new File(
-			"resources/constraints/university_complex.ocl");
+	final static File universityModel =
+			new File("resources/model/university_complex.uml");
+	final static File universityConstraints =
+			new File("resources/constraints/university_complex.ocl");
 
 	public static void main(String[] args) throws Exception {
 
-		StandaloneFacade.INSTANCE.initialize(new URL("file:"
-				+ new File("log4j.properties").getAbsolutePath()));
+		StandaloneFacade.INSTANCE.initialize(
+				new URL("file:" + new File("log4j.properties").getAbsolutePath()));
 
 		royalsAndLoyals();
 
@@ -85,11 +81,11 @@ public class StandaloneDresdenOCLExample {
 		System.out.println();
 
 		try {
-			IModel model = StandaloneFacade.INSTANCE.loadUMLModel(rlModel,
-					getUMLResources());
+			IModel model =
+					StandaloneFacade.INSTANCE.loadUMLModel(rlModel, getUMLResources());
 
-			IModelInstance modelInstance = StandaloneFacade.INSTANCE
-					.loadJavaModelInstance(model, rlInstance);
+			IModelInstance modelInstance =
+					StandaloneFacade.INSTANCE.loadJavaModelInstance(model, rlInstance);
 
 			List<Constraint> constraintList = StandaloneFacade.INSTANCE
 					.parseOclConstraints(model, rlOclConstraints);
@@ -102,8 +98,8 @@ public class StandaloneDresdenOCLExample {
 			long end = System.currentTimeMillis();
 
 			for (IInterpretationResult result : results) {
-				System.out.println("  " + result.getModelObject() + ": "
-						+ result.getResult());
+				System.out.println(
+						"  " + result.getModelObject() + ": " + result.getResult());
 			}
 
 			System.out.println("time for interpretation: " + (end - start));
@@ -116,21 +112,19 @@ public class StandaloneDresdenOCLExample {
 			System.out.println("---------------------------------");
 			System.out.println();
 
-			IOcl2JavaSettings settings = Ocl2JavaFactory.getInstance()
-					.createJavaCodeGeneratorSettings();
+			IOcl2JavaSettings settings =
+					Ocl2JavaFactory.getInstance().createJavaCodeGeneratorSettings();
 			settings.setSourceDirectory("src-gen/rl/");
 
 			System.out.println("Generate aspectJ files to "
 					+ settings.getSourceDirectory() + " ...");
-			StandaloneFacade.INSTANCE.generateAspectJCode(constraintList,
-					settings);
+			StandaloneFacade.INSTANCE.generateAspectJCode(constraintList, settings);
 			System.out.println("Finished code generation.");
 
 			System.out.println("Royals & Loyals - OCL Metrics");
 			System.out.println("---------------------------------");
 			System.out.println();
-			System.out.println(OclMetrics.computeMetric(constraintList)
-					.getReport());
+			System.out.println(OclMetrics.computeMetric(constraintList).getReport());
 			System.out.println();
 			System.out.println("Finished metrics computation.");
 		} catch (Exception e) {
@@ -148,28 +142,19 @@ public class StandaloneDresdenOCLExample {
 		System.out.println("-------------------------------");
 		System.out.println();
 
-		try {
-			IModel model = StandaloneFacade.INSTANCE.loadEcoreModel(pmlModel);
-
-			Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
-					"pml", new XMIResourceFactoryImpl());
-			PmlPackage.eINSTANCE.eClass();
-
-			IModelInstance modelInstance = StandaloneFacade.INSTANCE
-					.loadEcoreModelInstance(model, pmlInstance);
-
-			List<Constraint> constraintList = StandaloneFacade.INSTANCE
-					.parseOclConstraints(model, pmlOclConstraints);
-
-			for (IInterpretationResult result : StandaloneFacade.INSTANCE
-					.interpretEverything(modelInstance, constraintList)) {
-				System.out.println("  " + result.getModelObject() + ": "
-						+ result.getResult());
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		/*
+		 * try { IModel model = StandaloneFacade.INSTANCE.loadEcoreModel(pmlModel);
+		 * Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put( "pml",
+		 * new XMIResourceFactoryImpl()); PmlPackage.eINSTANCE.eClass();
+		 * IModelInstance modelInstance = StandaloneFacade.INSTANCE
+		 * .loadEcoreModelInstance(model, pmlInstance); List<Constraint>
+		 * constraintList = StandaloneFacade.INSTANCE .parseOclConstraints(model,
+		 * pmlOclConstraints); for (IInterpretationResult result :
+		 * StandaloneFacade.INSTANCE .interpretEverything(modelInstance,
+		 * constraintList)) { System.out.println("  " + result.getModelObject() +
+		 * ": " + result.getResult()); } } catch (Exception e) {
+		 * e.printStackTrace(); }
+		 */
 	}
 
 	private static void simple() {
@@ -182,53 +167,30 @@ public class StandaloneDresdenOCLExample {
 		System.out.println("--------------");
 		System.out.println();
 
-		try {
-			IModel model = StandaloneFacade.INSTANCE.loadJavaModel(simpleModel);
-
-			// create an empty model instance and put objects into it
-			IModelInstance modelInstance = new JavaModelInstance(model);
-
-			Person student = new Student();
-			student.setName("Student-work-a-lot");
-			student.setAge(23);
-
-			Person prof = new Professor();
-			prof.setName("Prof. Invalid");
-			prof.setAge(-42);
-
-			modelInstance.addModelInstanceElement(student);
-			modelInstance.addModelInstanceElement(prof);
-
-			List<Constraint> constraintList = StandaloneFacade.INSTANCE
-					.parseOclConstraints(model, simpleOclConstraints);
-
-			for (IInterpretationResult result : StandaloneFacade.INSTANCE
-					.interpretEverything(modelInstance, constraintList)) {
-				System.out.println("  " + result.getModelObject() + " ("
-						+ result.getConstraint().getKind() + ": "
-						+ result.getConstraint().getSpecification().getBody()
-						+ "): " + result.getResult());
-			}
-
-			IOcl2JavaSettings settings = Ocl2JavaFactory.getInstance()
-					.createJavaCodeGeneratorSettings();
-			settings.setSourceDirectory("src-gen/simple/");
-
-			System.out.println();
-			System.out.println("Generate Java Fragements:");
-			System.out.println("-------------------------");
-
-			for (String fragment : StandaloneFacade.INSTANCE.generateJavaCode(
-					constraintList, settings)) {
-				System.out.println(fragment);
-				System.out.println();
-
-			}
-			// end for.
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		/*
+		 * try { IModel model =
+		 * StandaloneFacade.INSTANCE.loadJavaModel(simpleModel); // create an empty
+		 * model instance and put objects into it IModelInstance modelInstance = new
+		 * JavaModelInstance(model); Person student = new Student();
+		 * student.setName("Student-work-a-lot"); student.setAge(23); Person prof =
+		 * new Professor(); prof.setName("Prof. Invalid"); prof.setAge(-42);
+		 * modelInstance.addModelInstanceElement(student);
+		 * modelInstance.addModelInstanceElement(prof); List<Constraint>
+		 * constraintList = StandaloneFacade.INSTANCE .parseOclConstraints(model,
+		 * simpleOclConstraints); for (IInterpretationResult result :
+		 * StandaloneFacade.INSTANCE .interpretEverything(modelInstance,
+		 * constraintList)) { System.out.println("  " + result.getModelObject() +
+		 * " (" + result.getConstraint().getKind() + ": " +
+		 * result.getConstraint().getSpecification().getBody() + "): " +
+		 * result.getResult()); } IOcl2JavaSettings settings =
+		 * Ocl2JavaFactory.getInstance() .createJavaCodeGeneratorSettings();
+		 * settings.setSourceDirectory("src-gen/simple/"); System.out.println();
+		 * System.out.println("Generate Java Fragements:");
+		 * System.out.println("-------------------------"); for (String fragment :
+		 * StandaloneFacade.INSTANCE.generateJavaCode( constraintList, settings)) {
+		 * System.out.println(fragment); System.out.println(); } // end for. } catch
+		 * (Exception e) { e.printStackTrace(); }
+		 */
 	}
 
 	private static void pain() {
@@ -244,16 +206,16 @@ public class StandaloneDresdenOCLExample {
 		try {
 			IModel model = StandaloneFacade.INSTANCE.loadXSDModel(painModel);
 
-			IModelInstance modelInstance = StandaloneFacade.INSTANCE
-					.loadXMLModelInstance(model, painInstance);
+			IModelInstance modelInstance =
+					StandaloneFacade.INSTANCE.loadXMLModelInstance(model, painInstance);
 
 			List<Constraint> constraintList = StandaloneFacade.INSTANCE
 					.parseOclConstraints(model, painOclConstraints);
 
 			for (IInterpretationResult result : StandaloneFacade.INSTANCE
 					.interpretEverything(modelInstance, constraintList)) {
-				System.out.println("  " + result.getModelObject() + ": "
-						+ result.getResult());
+				System.out.println(
+						"  " + result.getModelObject() + ": " + result.getResult());
 			}
 
 		} catch (Exception e) {
@@ -273,17 +235,17 @@ public class StandaloneDresdenOCLExample {
 		System.out.println();
 
 		try {
-			IModel model = StandaloneFacade.INSTANCE.loadUMLModel(
-					universityModel, getUMLResources());
+			IModel model = StandaloneFacade.INSTANCE.loadUMLModel(universityModel,
+					getUMLResources());
 
 			List<Constraint> constraintList = StandaloneFacade.INSTANCE
 					.parseOclConstraints(model, universityConstraints);
 
-			IOcl2DeclSettings settings = Ocl2DeclCodeFactory.getInstance()
-					.createOcl2DeclCodeSettings();
+			IOcl2DeclSettings settings =
+					Ocl2DeclCodeFactory.getInstance().createOcl2DeclCodeSettings();
 			settings.setSourceDirectory("src-gen/university/");
 			settings.setModus(Ocl2DeclSettings.MODUS_TYPED);
-			settings.setSaveCode(true);
+			// settings.setSaveCode(true);
 			settings.setTemplateGroup(TemplatePlugin.getTemplateGroupRegistry()
 					.getTemplateGroup("Standard(SQL)"));
 			StandaloneFacade.INSTANCE.generateSQLCode(constraintList, settings,
@@ -291,7 +253,7 @@ public class StandaloneDresdenOCLExample {
 
 			System.out.println("Finished code generation.");
 
-			settings.setSaveCode(false);
+			// settings.setSaveCode(false);
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -300,6 +262,7 @@ public class StandaloneDresdenOCLExample {
 	}
 
 	private static File getUMLResources() {
+
 		return new File(
 				"lib/org.eclipse.uml2.uml.resources_3.1.0.v201005031530.jar");
 	}
